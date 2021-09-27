d268702c50f468e56eb3524afcc8d6d2a59a7aa8
akushla <akushla@hetnet.nl>
2021-09-27 16:18:50 +0200

    eerste versie

---
+++ script.R
@@ -0,0 +1,25 @@
+install.packages("janitor")
+library ("tidyverse")
+library ("tidymodels")
+projdir <- getwd()
+setwd("C:/Users/akush/Downloads")
+raw <- read.csv2("bbga_latest_and_greatest.csv")
+
+gebied <- read.csv2("gebied.csv")
+gc()
+
+setwd (projdir)
+dataAms <- pivot_wider(raw, names_from = variabele, values_from = waarde)
+gc()
+test <- merge(gebied,dataAms, by="gebiedcode15")
+
+
+dataAms[3:ncol(dataAms)]<-sapply(dataAms[3:ncol(dataAms)], as.numeric)
+names(dataAms) <-names(janitor::clean_names(dataAms))
+
+
+test <- subset(dataAms, nchar(as.character(gebiedcode15)) == 3)
+
+data1 <- test
+data1        
+View(data1)
